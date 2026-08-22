778946738f93ebdbb9fe8db0fe3b6033847fc388
darkironman-collab <darkironman@kpgs.xyz>
2026-08-22 17:11:09 +0530

    Add transparent Extreme settings root icon

---
+++ extensions/extension/src/main/java/app/extremetube/extension/TransparentDrawable.java
@@ -0,0 +1,40 @@
+package app.extremetube.extension;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.Nullable;
+
+/** Invisible drawable used to remove the Morphe M icon beside the Extreme settings entry. */
+@SuppressWarnings("unused")
+public final class TransparentDrawable extends Drawable {
+    @Override
+    public void draw(Canvas canvas) {
+        // Deliberately empty.
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSPARENT;
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+        return 1;
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+        return 1;
+    }
+}
